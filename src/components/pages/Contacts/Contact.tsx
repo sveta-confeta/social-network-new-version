b2f68dc98f5d4b4585fca3afb1c2fadc4d9daa66
sveta-confeta <_svet_@tut.by>
2022-03-30 10:09:45 +0300

    create component Contact/ useSelector, useDispatch

--- src/components/pages/Contacts/Contact.tsx
+++ src/components/pages/Contacts/Contact.tsx
@@ -1,10 +1,83 @@
 import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {AppRootStateType} from "../../../redux/redux-store";
+import {ContactsType, followAC, setUsersAC, unFollowAC} from "../../../reducer/contactReducer";
+import s from './Contact.module.css'
+import {v1} from "uuid";
 
- export const Contact = () => {
-    return (
-        <div>
+export const Contact = () => {
+    const contacts = useSelector<AppRootStateType, Array<ContactsType>>(state => state.contactsPage.contacts);
+    const dispatch=useDispatch();
 
-        </div>
-    );
-};
+    const unfollowHandler=(userID:string)=>{
+        dispatch(unFollowAC(userID))
+    }
+
+    const followHandler=(userID:string)=>{
+        dispatch(followAC(userID))
+    }
+    if(contacts.length===0) {
+        dispatch(setUsersAC([
+            {
+                id: v1(),
+                fotoIcon: 'https://sun9-1.userapi.com/c855724/v855724535/15854/dHNLGjoiM_0.jpg',
+                followed: true,
+                fullName: 'Dmitry',
+                status: 'I am boss',
+                location: {city: 'Tbilisi', coutntry: 'Gorgia'}
+            },
+            {
+                id: v1(),
+                fotoIcon: 'https://sun9-1.userapi.com/c855724/v855724535/15854/dHNLGjoiM_0.jpg',
+                followed: true,
+                fullName: 'Sveta',
+                status: 'I am junior',
+                location: {city: 'Minsk', coutntry: 'Belarus'}
+            },
+            {
+                id: v1(),
+                fotoIcon: 'https://sun9-1.userapi.com/c855724/v855724535/15854/dHNLGjoiM_0.jpg',
+                followed: true,
+                fullName: 'Olga',
+                status: 'I am fine',
+                location: {city: 'Varshava', coutntry: 'Polska'}
+            },
+            {
+                id: v1(),
+                fotoIcon: 'https://sun9-1.userapi.com/c855724/v855724535/15854/dHNLGjoiM_0.jpg',
+                followed: false,
+                fullName: 'Natasha',
+                status: 'Shit happens',
+                location: {city: 'Moskow', coutntry: 'Rasha'}
+            }
+        ]))
+    }
+
+    return  <div>
+        {
+            contacts.map(m => <div className={s.bodyContacts} key={m.id}>
+                    <div className={s.icon}>
+                        <img className={s.ava} src={m.photos.small !== null ? m.photos.small : userPfoto}/> <br/>
+                        {m.followed ? <button onClick={()=>unfollowHandler(m.id)}>Unfolow</button>: <button onClick={()=>followHandler(m.id)}>Follow</button> }
+                        {/*//если followed -тру, тогда пусть будет кнопка которая сменит его состояние на фалсе:*/}
+                        {/*//  <button onClick={()=>unfollowHandler(m.id)}, в инном случае покажи кнопку followHandler(m.id)}*/}
+                    </div>
+
+                    <div className={s.info}>
+                        <div className={s.infoText}>
+                            <div className={s.nameUser}>{m.name}</div>
+                            <p className={s.status}>{m.status}</p>
+                        </div>
+
+                        <div className={s.country}>{'m.location.coutntry'}</div>
+                        <div className={s.city}>{'m.location.city'}</div>
+
+                    </div>
+
+                </div>
+            )}
+    </div>
+
+
+            };
 
--- src/reducer/contactReducer.ts
+++ src/reducer/contactReducer.ts
@@ -9,51 +9,73 @@ type setUsersACType= ReturnType<typeof setUsersAC>
 
 type ActionType = followACType | unFollowACType  | setUsersACType;
 
-const initialState={
-   contacts:[ {
-            id: v1(),
-            fotoIcon:'https://sun9-1.userapi.com/c855724/v855724535/15854/dHNLGjoiM_0.jpg',
-            followed: true,
-            fullName: 'Dmitry',
-            status: 'I am boss',
-            location: {city: 'Tbilisi', coutntry: 'Gorgia'}
-        },
-        {
-            id: v1(),
-            fotoIcon:'https://sun9-1.userapi.com/c855724/v855724535/15854/dHNLGjoiM_0.jpg',
-            followed: true,
-            fullName: 'Sveta',
-            status: 'I am junior',
-            location: {city: 'Minsk', coutntry: 'Belarus'}
-        },
-        {
-            id: v1(),
-            fotoIcon:'https://sun9-1.userapi.com/c855724/v855724535/15854/dHNLGjoiM_0.jpg',
-            followed: true,
-            fullName: 'Olga',
-            status: 'I am fine',
-            location: {city: 'Varshava', coutntry: 'Polska'}
-        },
-        {
-            id: v1(),
-            fotoIcon:'https://sun9-1.userapi.com/c855724/v855724535/15854/dHNLGjoiM_0.jpg',
-            followed: false,
-            fullName: 'Natasha',
-            status: 'Shit happens',
-            location: {city: 'Moskow', coutntry: 'Rasha'}
-        },]
+
+export type ContactsType = {
+
+    id: string
+    fotoIcon:string
+    // photos: {
+    //     small: undefined|string,
+    //     large: undefined|string,
+    // }
+    followed: boolean
+    fullName: string
+    status: string
+    location: { city: string, coutntry: string }
 }
 
-export const contactsReducer=(state:=initialState,action:ActionType)=>{
+export type ContactsStateType = {
+    contacts: Array<ContactsType>
+}
+
+const initialState:ContactsStateType={
+    contacts:[ //данные будут браться с сервера, изначально массив пустой
+        //{
+   //          id: v1(),
+   //          fotoIcon:'https://sun9-1.userapi.com/c855724/v855724535/15854/dHNLGjoiM_0.jpg',
+   //          followed: true,
+   //          fullName: 'Dmitry',
+   //          status: 'I am boss',
+   //          location: {city: 'Tbilisi', coutntry: 'Gorgia'}
+   //      },
+   //      {
+   //          id: v1(),
+   //          fotoIcon:'https://sun9-1.userapi.com/c855724/v855724535/15854/dHNLGjoiM_0.jpg',
+   //          followed: true,
+   //          fullName: 'Sveta',
+   //          status: 'I am junior',
+   //          location: {city: 'Minsk', coutntry: 'Belarus'}
+   //      },
+   //      {
+   //          id: v1(),
+   //          fotoIcon:'https://sun9-1.userapi.com/c855724/v855724535/15854/dHNLGjoiM_0.jpg',
+   //          followed: true,
+   //          fullName: 'Olga',
+   //          status: 'I am fine',
+   //          location: {city: 'Varshava', coutntry: 'Polska'}
+   //      },
+   //      {
+   //          id: v1(),
+   //          fotoIcon:'https://sun9-1.userapi.com/c855724/v855724535/15854/dHNLGjoiM_0.jpg',
+   //          followed: false,
+   //          fullName: 'Natasha',
+   //          status: 'Shit happens',
+   //          location: {city: 'Moskow', coutntry: 'Rasha'}
+   //      }
+    ]
+
+}
+
+export const contactsReducer=(state:ContactsStateType=initialState,action:ActionType):ContactsStateType=>{
     switch (action.type) {
         case 'FOLLOW': {
-            return {...state,contact:state.contact.map(m=> m.id===action.userID ? {...m,followed:true} : m)}
+            return {...state,contacts:state.contacts.map(m=> m.id===action.userID ? {...m,followed:true} : m)} //меняет с false на true
         }
         case 'UN-FOLLOW':{
-            return {...state,contact:state.contact.map(m=> m.id===action.userID  ?{...m,followed:false} : m)}
+            return {...state,contacts:state.contacts.map(m=> m.id===action.userID  ?{...m,followed:false} : m)}//меняет с  true на false
         }
         case 'SET-USERS':{
-            return {...state,contact: [...state.contact, ...action.users]}
+            return {...state,contacts: [...state.contacts, ...action.users]} //добавляем юзеров в пустой массив, добавляем к уже существующим(склеиваем два массива)
         }
         default:
             return  state;
@@ -73,7 +95,7 @@ export const unFollowAC = (userID: string) => { //чтоб определить 
         userID,
     } as const
 }
-// // c cервера будем брать юзеров и помещать в обьект=стейт:
+// // c cервера будем брать юзеров и добавлять в обьект=стейт:
 export const setUsersAC = (users:Array<ContactsType>) => {
     return {
         type: 'SET-USERS',
